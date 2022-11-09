5468d4490dc5487293f6bf43abb2270e9e77f9b8
Salmalisa <salmalisa7342@gmail.com>
2022-11-09 07:37:39 +0600

    services api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -16,10 +16,37 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t3mwvsa.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log('mongo connected')
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+  try {
+    const serviceCollection = client.db('laraCriptonDb').collection('services')
+    app.get('/limitedServices', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query)
+      const result = await cursor.limit(3).toArray()
+      res.send(result)
+    })
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id:ObjectId(id)};
+      const result = await serviceCollection.findOne(query)
+      console.log(result)
+      res.send(result)
+    })
+  }
+  finally {
+    
+  }
+}
+run().catch(error =>console.log(error))
 
 app.listen(port, () => {
   console.log('server is running at port',port)
