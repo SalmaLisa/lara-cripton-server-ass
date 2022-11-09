8fffadfcc411b358aacc4497888d04be29cedf99
Salmalisa <salmalisa7342@gmail.com>
2022-11-09 18:22:59 +0600

    delete operation implemented

--- index.js
+++ index.js
@@ -68,6 +68,12 @@ async function run() {
       const filteredReviews = reviews.filter(review=>review.serviceId===id)
       res.send(filteredReviews)
     })
+    app.delete('/reviews/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await reviewCollection.deleteOne(query)
+      res.send(result)
+    })
     
   }
   finally {
